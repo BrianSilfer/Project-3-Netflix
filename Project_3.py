3e89e2467a3d3d98200d0d0ecaaec9efa57e9a53
BrianSilfer <briansilfer67@gmail.com>
2023-07-24 21:14:38 -0400

    update

--- Project_3.py
+++ Project_3.py
@@ -1,12 +1,23 @@
-# Import Yahoo Finance and JSON Dependency 
-import yfinance as yf
-import json
-#define variable to store Netflix stock data
-nflx = yf.Ticker("NFLX")
-
-#print results
-#print(nflx.info)
-
-#convert dictionary to JSON
-nflx_json = json.dumps(nflx,indent = 4, sort_keys=True)
-print(nflx_json)
+from flask import Flask, jsonify
+import psycopg2
+app = Flask(__name__)
+def connect_db():
+    conn = psycopg2.connect(
+        dbname='Project_3',
+        user='postgres',
+        password='bsilf6767',
+        host='localhost',
+        port='5432'
+    )
+    return conn
+@app.route('/data', methods=['GET'])
+def get_data():
+    conn = connect_db()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM netflix_titles')
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(rows)
+if __name__ == '__main__':
+    app.run(debug=True) 

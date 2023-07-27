6784ae0469511d5706641302bdfe9c4ffc4841ed
BrianSilfer <briansilfer67@gmail.com>
2023-07-27 19:28:55 -0400

    update

---
+++ Project_3.js
@@ -0,0 +1,8 @@
+//Retreive Data from API endpoint
+
+d3.json("http://127.0.0.1:5000/movies").then((data1) => {
+    //check that data loaded
+    console.log(data1)
+    
+
+});
--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 function init() {
-  
+  //pulling movies data from
+  d3.json("http://127.0.0.1:5000/movies").then((titles_data) => {
+    //check that data loaded
+    console.log(titles_data)
     
+    for 
+});
   }
     
-  init();
+init();
